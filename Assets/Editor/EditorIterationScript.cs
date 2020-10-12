0c72b02489efbcc82c183d37e4c9bfe6bac3189a
Steven <thevgm01@gmail.com>
2020-10-12 18:15:03 -0500

    Added in-editor scripts to iterate and mirror transforms repeatedly

---
+++ Assets/Editor/EditorIterationScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(IterationScript))]
+public class EditorIterationScript : Editor
+{
+    Transform obj;
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        IterationScript cube = (IterationScript)target;
+
+        GUILayout.Space(20);
+
+        GUILayout.Label("Iteration");
+        GUILayout.BeginHorizontal();
+            if(GUILayout.Button("Iterate")) cube.Iterate();
+            if(GUILayout.Button("Reset Iterations")) cube.ResetIterations();
+        GUILayout.EndHorizontal();
+        GUILayout.Label("Mirroring Axis");
+        GUILayout.BeginHorizontal();
+            if (GUILayout.Button("X")) cube.Mirror(new Vector3(-1, 1, 1));
+            if (GUILayout.Button("Y")) cube.Mirror(new Vector3(1, -1, 1));
+            if (GUILayout.Button("Z")) cube.Mirror(new Vector3(1, 1, -1));
+        GUILayout.EndHorizontal();
+        GUILayout.BeginHorizontal();
+            if (GUILayout.Button("XY")) cube.Mirror(new Vector3(-1, -1, 1));
+            if (GUILayout.Button("YZ")) cube.Mirror(new Vector3(1, -1, -1));
+            if (GUILayout.Button("ZX")) cube.Mirror(new Vector3(-1, 1, -1));
+        GUILayout.EndHorizontal();
+        GUILayout.BeginHorizontal();
+            if (GUILayout.Button("XYZ")) cube.Mirror(new Vector3(-1, -1, -1));
+        GUILayout.EndHorizontal();
+        EditorGUILayout.HelpBox("This script makes copies of objects that change slightly with each copy. " +
+            "Make sure the \"Relative Iteration Transform\" is close to the parent transform. " +
+            "Mirroring will copy the current state of the iteration, and further mirrors will operate on existing ones too.", MessageType.None);
+    }
+}
---
+++ Assets/IterationScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IterationScript : MonoBehaviour
+{
+    public Transform relativeIterationTransform;
+    public PrimitiveType iterationPrimitive;
+    private Transform iterationContainer;
+    private Transform lastIteration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void Iterate()
+    {
+        if (lastIteration == null)
+        {
+            iterationContainer = new GameObject().transform;
+            iterationContainer.gameObject.name = "Iterations";
+            iterationContainer.parent = transform;
+            lastIteration = transform;
+        }
+
+        var sphere = GameObject.CreatePrimitive(iterationPrimitive).transform;
+        sphere.parent = lastIteration;
+        sphere.localPosition = relativeIterationTransform.position - transform.position;
+        sphere.localRotation = relativeIterationTransform.rotation * Quaternion.Inverse(transform.rotation);
+        sphere.localScale = relativeIterationTransform.localScale;
+        sphere.parent = iterationContainer;
+        lastIteration = sphere;
+    }
+
+    public void ResetIterations()
+    {
+        if(Application.isPlaying) Destroy(iterationContainer.gameObject);
+        else DestroyImmediate(iterationContainer.gameObject);
+    }
+
+    public void Mirror(Vector3 scale)
+    {
+        var mirrored = Instantiate(iterationContainer);
+        mirrored.localScale = scale;
+        mirrored.parent = iterationContainer;
+    }
+}
